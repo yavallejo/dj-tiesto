4e388411490da5917d7599df1401dc7e001ada8a
yan arlex vallejo <aprendiendoandoo@gmail.com>
2019-06-14 12:00:27 -0500

    Hero image ACF page options

--- header.php
+++ header.php
@@ -9,7 +9,11 @@
     
 </head>
 <body>
-    <header class="header">
+    <header class="header"
+        <?php if ( get_field('po_slider_imagen_principal', 'option') ) : ?>            
+            style="background-image: url('<?php echo get_field('po_slider_imagen_principal', 'option'); ?>');"
+        <?php endif; ?>
+     >
         <div class="container pt-1 pt-md-4 header__top">
             <nav class="navbar navbar-expand-lg">
                 <a class="navbar-brand" href="index.html">
@@ -47,20 +51,45 @@
         </div>
         <div class="container header__bottom">
             <ul class="header__meta list-inline d-flex">
-                <li><img src="<?php bloginfo('template_directory');?>/assets/img/icon_date.png" alt="21 Sept 2018"> 21 Sept 2018</li>
-                <li><img src="<?php bloginfo('template_directory');?>/assets/img/icon_headphones.png" alt="24 Djs"> 24 Djs</li>
-                <li><img src="<?php bloginfo('template_directory');?>/assets/img/icon_ticket.png" alt="3000 Entradas"> 3000 Entradas</li>
+                <?php if ( get_field('po_slider_fecha_del_evento', 'option') ) : ?>                    
+                    <li>
+                        <img src="<?php bloginfo('template_directory');?>/assets/img/icon_date.png" alt="<?php echo get_field('po_slider_fecha_del_evento', 'option'); ?>"> <?php echo get_field('po_slider_fecha_del_evento', 'option'); ?>
+                    </li>
+                <?php endif; ?>
+                
+                <?php if ( get_field('po_slider_cantidad_de_djs', 'option') ) : ?>
+                    <li>
+                        <img src="<?php bloginfo('template_directory');?>/assets/img/icon_headphones.png" alt="<?php echo get_field('po_slider_cantidad_de_djs', 'option'); ?>"> <?php echo get_field('po_slider_cantidad_de_djs', 'option'); ?> Djs
+                    </li>
+                <?php endif; ?>
+
+                <?php if ( get_field('po_slider_entradas_disponibles', 'option') ) : ?>
+                    
+                    <li>
+                        <img src="<?php bloginfo('template_directory');?>/assets/img/icon_ticket.png" alt="<?php echo get_field('po_slider_entradas_disponibles', 'option'); ?>"> <?php echo get_field('po_slider_entradas_disponibles', 'option'); ?> Entradas
+                    </li>
+                <?php endif; ?>
             </ul>
-            <div class="header__information">
-                <h2>
-                    <strong>WP</strong> Festival Music <strong>Bogotá</strong> 2018
-                </h2>
-                <p class="mb-0 mt-4 mt-md-0 pl-3 pr-3 p-md-0">Bogotá <strong>WP Festival Music</strong> es un Evento de música electrónica con Djs que estan dentro del Top 20 en el mundo.
-                </p>
+            <div class="header__information">            
+                <?php if ( get_field('po_slider_titulo_del_evento', 'option') ) : ?>
+                    <h2>
+                        <?php echo get_field('po_slider_titulo_del_evento', 'option'); ?>
+                    </h2>
+                <?php endif; ?>
+
+                <?php if ( get_field('po_slider_descripcion', 'option') ) : ?>
+                    <p class="mb-0 mt-4 mt-md-0 pl-3 pr-3 p-md-0">
+                        <?php echo get_field('po_slider_descripcion', 'option'); ?>
+                    </p>
+                <?php endif; ?> 
+
                 <p class="d-none d-md-block mt-0">Luces, buena música y los mejores Djs.</p>
-                <div class="header__btn">
-                    <a href="#" class="btn btn-primary btn-lg mt-5 mt-md-3">Comprar Tickets</a>
-                </div>
+                
+                <?php if ( get_field('po_slider_link_compra', 'option') ) : ?>
+                    <div class="header__btn">
+                        <a href="<?php echo get_field('po_slider_link_compra', 'option'); ?>" class="btn btn-primary btn-lg mt-5 mt-md-3">Comprar Tickets</a>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </header> <!--Header-->
