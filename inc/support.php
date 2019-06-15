e7633a9a0b7b9f81d63033b06f7ac4bc7e06eeb5
yan arlex vallejo <aprendiendoandoo@gmail.com>
2019-06-15 11:46:27 -0500

    Add image size, the post thumbnail

--- inc/support.php
+++ inc/support.php
@@ -19,7 +19,15 @@ function djtiesto_custom_logo(){
 add_action( 'after_setup_theme', 'djtiesto_custom_logo' );
 
 // HTML5
+
 add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
 // Title Tag
+
 add_theme_support( 'title-tag' );
+
+// Add image size
+
+add_image_size( 'sincortar', 220, 180 );
+add_image_size( 'cortar', 300, 300, true );
+add_image_size( 'cortarlefttop', 500, 500, array( 'right', 'bottom' ) );
--- single.php
+++ single.php
@@ -1,1 +1,14 @@
-Hola soy una noticvia
+
+
+<h1><?php the_title(); ?></h1>
+<?php
+
+
+
+if ( has_post_thumbnail() ) { 
+    the_post_thumbnail( 'cortarlefttop' );
+}else{
+    ?>
+    <img class="img-fluid" src="<?php bloginfo('template_directory');?>/assets/img/icon_instagram.png" alt="Logo Default">
+    <?php
+}
