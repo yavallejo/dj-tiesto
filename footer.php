254dac85c0b4a56937c02e66cae2ecb8a0c8fe4d
Yan Arlex <yavallejor@gmail.com>
2019-07-01 12:03:57 -0500

    footer

--- footer.php
+++ footer.php
@@ -2,18 +2,36 @@
         <div class="container-fluid pl-0 pr-0">
             <div class="row">
                 <div class="col-lg-6">
+
+
                     <div class="d-flex flex-column justify-content-center footer__information text-center text-lg-left">
-                        <h5>WP FESTIVAL MUSIC - Bogotá</h5>
-                        <p class="d-none d-md-block mt-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                            nonumy eirmod
-                            tempor invidunt ut labore et dolore
-                            magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-                            rebum.</p>
+                        <h5><?php the_field('po_footer_titulo','option') ?> </h5>
+                        <p class="d-none d-md-block mt-3">
+                           <?php the_field('po_footer_descripción','option'); ?>
+                        </p>
                         <ul class="list-unstyled d-flex justify-content-center justify-content-lg-start mt-5">
-                            <li class="mr-4"><a href=""><img src="<?php bloginfo('template_directory');?>/assets/img/icon_twitter.png" alt=""></a></li>
-                            <li class="mr-4"><a href=""><img src="<?php bloginfo('template_directory');?>/assets/img/icon_facebook.png" alt=""></a></li>
-                            <li class="mr-4"><a href=""><img src="<?php bloginfo('template_directory');?>/assets/img/icon_instagram.png" alt=""></a></li>
+                            <?php if ( have_rows('po_rp_footer_redes_sociales','option') ) : ?>
+                            
+                                <?php while( have_rows('po_rp_footer_redes_sociales','option') ) : the_row(); ?>
+                            
+                                    <li class="mr-4">
+                                        <a href="<?php the_sub_field('po_rp_footer_link','option');?>" target="_blank">
+                                            <img src="<?php the_sub_field('po_rp_footer_icono','option');?>" alt="<?php the_sub_field('po_rp_footer_icono','option');?>">
+                                        </a>
+                                    </li>
+                            
+                                <?php endwhile; ?>
+                            
+                            <?php endif; ?>
+                            
+                            
+                            
                         </ul>
+
+
+
+
+
                         <nav class="footer__nav">
                             <ul class="list-unstyled  d-flex justify-content-center justify-content-lg-start">
                                 <li class="mr-3"><a href="#">WP FESTIVAL</a></li>
